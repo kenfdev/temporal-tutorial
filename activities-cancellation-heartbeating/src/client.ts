f254c042c3f35eb889b5123960df2a1048f1138f
Ken Fukuyama <kenfdev@gmail.com>
2023-02-03 07:42:58 +0000

    add activities-cancellation-heartbeating

---
+++ activities-cancellation-heartbeating/src/client.ts
@@ -0,0 +1,34 @@
+import { Connection, Client, WorkflowFailedError, CancelledFailure } from '@temporalio/client';
+import { runCancellableActivity } from './workflows';
+
+async function run() {
+  const connection = await Connection.connect({
+    address: 'temporal:7233',
+  });
+  const client = new Client({ connection });
+
+  const handle = await client.workflow.start(runCancellableActivity, {
+    taskQueue: 'cancellation-heartbeating',
+    workflowId: 'cancellation-heartbeating-0',
+  });
+
+  // Simulate waiting for some time
+  // Cancel may be immediately called, waiting is not needed
+  await new Promise((resolve) => setTimeout(resolve, 40 * 1000));
+  await handle.cancel();
+  console.log('Cancelled workflow successfully');
+  try {
+    await handle.result();
+  } catch (err: unknown) {
+    if (err instanceof WorkflowFailedError && err.cause instanceof CancelledFailure) {
+      console.log('handle.result() threw because Workflow was cancelled');
+    } else {
+      throw err;
+    }
+  }
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
